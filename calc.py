cb7223be0ef9a7402f491ef98dc3e7a9ded0541a
Shilpajordan <Shilpa.nair@dci-student.org>
2023-11-20 15:36:59 +0100

    changes made

--- calc.py
+++ calc.py
@@ -1,20 +1,22 @@
-class Calculator:
+#class Calculator:
 
-    def add(self,x, y):
-        """ Add function"""
-        return x + y
-
-
-    def subtract(self,x , y):
-        """ Subtract function"""
-        return x - y
+def add(x, y):
+    """ Add function"""
+    return x + y
 
 
-    def multiply(self,x , y):
-        """ Multiply function"""
-        return x * y
+def subtract(x , y):
+    """ Subtract function"""
+    return x - y
 
 
-    def divide(self,x , y):
-        """ Divide function"""
-        return x / y
+def multiply(x , y):
+    """ Multiply function"""
+    return x * y
+
+
+def divide(x , y):
+    """ Divide function"""
+    if y == 0:
+        raise ValueError('Cannot divide by Zero')
+    return x / y
--- test_calc.py
+++ test_calc.py
@@ -20,6 +20,16 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.subtract(-1,-1), 0)
 
 
+    def test_multiply(self):
+        self.assertEqual(calc.subtract(10,5), 50)
+        self.assertEqual(calc.subtract(-1,1), -1)
+        self.assertEqual(calc.subtract(-1,-1), 1)
+
+    def test_divide(self):
+        self.assertEqual(calc.subtract(10,5), 2)
+        self.assertEqual(calc.subtract(-1,1), -2)
+        self.assertEqual(calc.subtract(-1,-1), 0)    
+
 
 if __name__ == '__main__':
-    unittest.main
+    unittest.main()
